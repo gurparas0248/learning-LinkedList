b82c037b82f15b1fc7a32d36a3944bcda290cf4e
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2023-01-09 18:15:41 +0530

    removing cycle in LL

--- Introduction/LinkedList.java
+++ Introduction/LinkedList.java
@@ -256,12 +256,40 @@ public class LinkedList {
         }
         return false;
     }
+
+    public static void removecycle(){
+        //detect cycle
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        //find meeting point
+        slow=head;
+        Node prev=null;//last node
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        //remove cycle-> last.next=null
+        prev.next=null;
+    }
     public static void main(String[] args) {
 
         head=new Node(1);
         head.next=new Node(2);
         head.next.next=new Node(3);
-        //head.next.next.next=new Node(4);
+        head.next.next.next=new Node(4);
         System.out.println(isCycle());
 
 
