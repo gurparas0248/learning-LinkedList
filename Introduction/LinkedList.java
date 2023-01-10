1f27d7fef90c3ea7a41c067d1046233cbeb2d207
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2023-01-10 19:11:06 +0530

    zigzag in LL

--- Introduction/LinkedList.java
+++ Introduction/LinkedList.java
@@ -337,6 +337,46 @@ public class LinkedList {
         //merge
         return merge(newLeft,newRight);
     }
+
+    public void zigZag(){
+
+        //find mid
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node mid=slow;
+
+        //reverse 2nd half
+        Node curr=mid.next;
+        mid.next=null;
+        Node prev=null;
+        Node next;
+
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        Node left=head;
+        Node right=prev;
+        Node nextL,nextR;
+
+        //alt merge -zig zag
+        while(left!=null && right!=null){
+            nextL=left.next;
+            left.next=right;
+            nextR=right.next;
+            right.next=nextL;
+
+            left=nextL;
+            right=nextR;
+        }
+    }
     public static void main(String[] args) {
 
         LinkedList ll=new LinkedList();
@@ -345,9 +385,11 @@ public class LinkedList {
         ll.addFirst(3);
         ll.addFirst(4);
         ll.addFirst(5);
+        ll.addFirst(6);
+
 
         ll.print();
-        ll.head=ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
 
     }
