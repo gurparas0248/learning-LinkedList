5091ce64ea5d79d81846f423aac2a84fe3fc00f1
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2023-01-10 19:18:20 +0530

    created a doubly LL class

---
+++ Introduction/DoublyLL.java
@@ -0,0 +1,23 @@
+package Introduction;
+
+public class DoublyLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node (int data){
+            this.data=data;
+            this.next=null;
+            this.prev=null;
+        }
+
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    
+    public static void main(String[] args) {
+
+    }
+}
